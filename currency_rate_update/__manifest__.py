63fa94a3c19354049202d0523db2bc7e1d64a3e4
IliaYusov <ouster2k@gmail.com>
2023-10-09 17:43:18 +0300

    currency rates update

---
+++ currency_rate_update/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name' : 'Currency_rate_update',
+    'version' : '1',
+    'category': 'Project',
+    'description':"""
+    """,
+    'assets': {
+        },
+    'data': [
+        'security/ir.model.access.csv',
+        'data/currency_rate_update_data.xml',
+        'wizard/currency_rate_update_wizard.xml',
+    ],
+    'demo':[
+
+    ],
+    'application':False,
+    'installable':True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
