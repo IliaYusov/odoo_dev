8b2cfbd84e5182f46cabd9679ba679fc55461f7c
IliaYusov <ouster2k@gmail.com>
2023-10-10 17:26:12 +0300

    forecasts for distributions

--- project_budget/models/distribution_cash.py
+++ project_budget/models/distribution_cash.py
@@ -27,7 +27,8 @@ class distribution_cash(models.Model):
     distribution_sum_with_vat_ostatok = fields.Monetary(related='planned_cash_flow_id.distribution_sum_with_vat_ostatok', string ="distribution_sum_with_vat_ostatok", readonly=True)
 
     sum_cash_without_vat = fields.Monetary(string="fact sum_cash_without_vat", required=True, copy=True, compute='_compute_sum')
-    sum_cash = fields.Monetary(string="fact sum_cash", required=True, copy=True)
+    sum_cash = fields.Monetary(string="fact sum_cash", compute='_compute_default_sum',
+                                           inverse='_inverse_compute_default_sum', store=True, required=True, copy=True)
 
     @api.depends("sum_cash")
     def _compute_sum(self):
@@ -37,3 +38,11 @@ class distribution_cash(models.Model):
             else:
                 row.sum_cash_without_vat = row.sum_cash / (1 + row.fact_cash_flow_id.projects_id.vat_attribute_id.percent / 100)
         # row.planned_cash_flow_id.compute_distribution_sum()
+
+    @api.depends("distribution_sum_with_vat_ostatok")
+    def _compute_default_sum(self):
+        for row in self:
+            row.sum_cash = min(row.distribution_sum_with_vat_ostatok, row.fact_cash_flow_id.sum_cash)
+
+    def _inverse_compute_default_sum(self):
+        pass
