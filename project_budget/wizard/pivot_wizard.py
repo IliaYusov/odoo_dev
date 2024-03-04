7d3855cd9fce4c87aa826fbc9249f086ffc209aa
IliaYusov <ouster2k@gmail.com>
2024-03-04 16:30:40 +0300

    get_view version of wizard

--- project_budget/wizard/pivot_wizard.py
+++ project_budget/wizard/pivot_wizard.py
@@ -4,15 +4,9 @@ class pivot_wizard(models.TransientModel):
     _name = 'project_budget.pivot_wizard'
     _description = 'Pivot Wizard'
 
-    name = fields.Char(string="Project_ID | Step ID",)
-    project_id = fields.Many2one('project_budget.projects')
-    step_id = fields.Many2one('project_budget.project_steps')
-    company_id = fields.Many2one('res.company', related='project_id.company_id', store=True)
-    project_office_id = fields.Many2one('project_budget.project_office', related='project_id.project_office_id', store=True)
-    total_amount_of_revenue = fields.Monetary(string='total_amount_of_revenue')
-    currency_id = fields.Many2one('res.currency', related='project_id.currency_id')
-
-    def create_fields(self):
+    @api.model
+    def get_view(self, view_id=None, view_type='pivot', **options):
+        res = super(pivot_wizard, self).get_view(view_id=view_id, view_type=view_type, **options)
         old_model = self.env['project_budget.pivot_wizard'].search([])
         if old_model:
             for spec in old_model:
@@ -25,6 +19,7 @@ class pivot_wizard(models.TransientModel):
                         'project_id': project.id,
                         'step_id': step.id,
                         'total_amount_of_revenue': step.total_amount_of_revenue,
+                        'end_presale_project_month': step.end_presale_project_month,
                     })
             else:
                 self.create({
@@ -32,12 +27,15 @@ class pivot_wizard(models.TransientModel):
                     'project_id': project.id,
                     'step_id': False,
                     'total_amount_of_revenue': project.total_amount_of_revenue,
+                    'end_presale_project_month': project.end_presale_project_month,
                 })
-        return {
-            'type': 'ir.actions.act_window',
-            'name': 'action_pivot_wizard_pivot',
-            'view_type': 'pivot',
-            'view_mode': 'pivot',
-            'res_model': 'project_budget.pivot_wizard',
-            'target': 'current',
-        }
+        return res
+
+    name = fields.Char(string="Project_ID | Step ID",)
+    project_id = fields.Many2one('project_budget.projects')
+    step_id = fields.Many2one('project_budget.project_steps')
+    company_id = fields.Many2one('res.company', related='project_id.company_id', store=True)
+    project_office_id = fields.Many2one('project_budget.project_office', related='project_id.project_office_id', store=True)
+    total_amount_of_revenue = fields.Monetary(string='total amount of revenue')
+    end_presale_project_month = fields.Date(string='project presale end month')
+    currency_id = fields.Many2one('res.currency', related='project_id.currency_id')
