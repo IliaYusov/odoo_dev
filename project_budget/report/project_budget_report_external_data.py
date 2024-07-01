214fa2963bdb010169e4686eebd0c0b2233887e6
IliaYusov <ouster2k@gmail.com>
2024-07-01 15:03:27 +0300

    second commit

--- project_budget/report/project_budget_report_external_data.py
+++ project_budget/report/project_budget_report_external_data.py
@@ -4,9 +4,9 @@ from odoo import fields, models
 class ReportExternalData(models.Model):
     _name = 'project_budget.report_external_data'
     _description = 'Report External Data'
+    _check_company_auto = True
 
-    company = fields.Char(string='Company')
+    company_id = fields.Many2one('res.company', string='Company', required=True)
+    report_date = fields.Datetime(string='Report Date', default=fields.datetime.now())
     data = fields.Text(string='Data')
-
-    def import_report_external_data(self):
-        pass
+    file = fields.Binary(string='File')
