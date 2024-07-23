e039012ce21d44c1496bf590dc2e70118b538bf7
IliaYusov <ouster2k@gmail.com>
2024-07-23 18:03:29 +0300

    added email attachment import

--- project_budget/report/project_budget_report_external_data.py
+++ project_budget/report/project_budget_report_external_data.py
@@ -1,13 +1,51 @@
 from odoo import fields, models
+from openpyxl import load_workbook
+from io import BytesIO
+import base64
+import datetime
 
 
 class ReportExternalData(models.Model):
     _name = 'project_budget.report_external_data'
     _description = 'Report External Data'
     _check_company_auto = True
-    _rec_name = 'report_date'
+    _inherit = ['mail.thread']
+
+    SUBJECT_STRING = '[Отчет для УК]'  # Тема письма "[Отчет для УК] Стэп Лоджик_23.07.2024"
 
     company_id = fields.Many2one('res.company', string='Company', required=True)
     report_date = fields.Date(string='Report Date', default=fields.date.today())
     data = fields.Text(string='Data')
     file = fields.Binary(string='File')
+
+    def name_get(self):
+        return [(record.id, f"{record.report_date}") for record in self]
+
+    def file_to_data(self, file):
+        wb = load_workbook(file, read_only=True, data_only=True)
+        ws = wb.active
+        data = []
+        for record in ws.iter_rows(min_row=6, max_row=6, min_col=4, max_col=None, values_only=True):
+            data = str(record).strip('()')
+        if data:
+            return data
+
+    def message_new(self, msg_dict, custom_values=None):
+        subject = msg_dict.get('subject', '')
+        attachments = msg_dict.get('attachments', '')
+        if self.SUBJECT_STRING in subject and attachments:
+            for attachment in attachments:
+                company_name, date = subject.strip('[Отчет для УК]').split('_')
+                data = self.file_to_data(BytesIO(attachment[1]))
+                if data:
+                    defaults = {
+                        'company_id': self.env['res.company'].search([('name', '=', company_name.strip())]).id,
+                        'data': data,
+                        'file': base64.b64encode(attachment[1]),
+                        'report_date': datetime.datetime.strptime(date.strip(), '%d.%m.%Y')
+                    }
+                    defaults.update(custom_values or {})
+                    res = super(ReportExternalData, self).message_new(msg_dict, custom_values=defaults)
+                    return res
+                else:
+                    continue
--- project_budget/wizard/open_file_wizard.py
+++ project_budget/wizard/open_file_wizard.py
@@ -1,5 +1,4 @@
 from odoo import models, fields, _
-from openpyxl import load_workbook
 from io import BytesIO
 import base64
 
@@ -11,15 +10,9 @@ class OpenFileWizard(models.TransientModel):
     company_id = fields.Many2one('res.company', string='Company', required=True)
     data_file = fields.Binary(string='Data file', required=True)
 
-    def import_file_data_from_wizard(self):
-        self.import_file_data(BytesIO(base64.b64decode(self.data_file)))
-
-    def import_file_data(self, file_name):
-        wb = load_workbook(file_name, read_only=True, data_only=True)
-        ws = wb.active
-        data = []
-        for record in ws.iter_rows(min_row=6, max_row=6, min_col=4, max_col=None, values_only=True):
-            data = str(record).strip('()')
+    def import_file_data(self):
+        file = BytesIO(base64.b64decode(self.data_file))
+        data = self.env['project_budget.report_external_data'].file_to_data(file)
         if data:
             self.env['project_budget.report_external_data'].create({
                 'company_id': self.company_id.id,
