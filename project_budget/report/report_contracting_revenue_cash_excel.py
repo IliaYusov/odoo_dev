2ed6dfd6cf92e9e084de8cccdf9e05a17cb32ccb
IliaYusov <ouster2k@gmail.com>
2025-03-10 17:09:22 +0300

    Added cash and revenue ids to Landata indicators report

--- project_budget/report/report_contracting_revenue_cash_excel.py
+++ project_budget/report/report_contracting_revenue_cash_excel.py
@@ -26,7 +26,7 @@ class ReportContractingRevenueCashExcel(models.AbstractModel):
         })
 
         head_schema = {
-            'common':
+            'contracting':
                 (
                     'ID проекта',
                     'Номер этапа проекта',
@@ -40,9 +40,6 @@ class ReportContractingRevenueCashExcel(models.AbstractModel):
                     'Наименование',
                     'Этап',
                     'Рентабельность',
-                ),
-            'contracting':
-                (
                     'Дата контрактования',
                     'Квартал контрактования',
                     'Сумма контрактования',
@@ -50,6 +47,19 @@ class ReportContractingRevenueCashExcel(models.AbstractModel):
                 ),
             'revenue':
                 (
+                    'ID проекта',
+                    'ID ВВ',
+                    'Номер этапа проекта',
+                    'Центр ответственности',
+                    'ПН',
+                    'Ответственный РПН',
+                    'КАМ',
+                    'Руководитель проекта',
+                    'Заказчик',
+                    'Партнер',
+                    'Наименование',
+                    'Этап',
+                    'Рентабельность',
                     'Дата ВВ',
                     'Квартал ВВ',
                     'Сумма ВВ',
@@ -58,6 +68,19 @@ class ReportContractingRevenueCashExcel(models.AbstractModel):
                 ),
             'cash':
                 (
+                    'ID проекта',
+                    'ID ПДС',
+                    'Номер этапа проекта',
+                    'Центр ответственности',
+                    'ПН',
+                    'Ответственный РПН',
+                    'КАМ',
+                    'Руководитель проекта',
+                    'Заказчик',
+                    'Партнер',
+                    'Наименование',
+                    'Этап',
+                    'Рентабельность',
                     'Дата ПДС',
                     'Квартал ПДС',
                     'Сумма ПДС',
@@ -70,15 +93,12 @@ class ReportContractingRevenueCashExcel(models.AbstractModel):
         for sheet_name, sheet in sheets.items():
             row = 0
             col = 0
-            for sc in head_schema['common']:
-                sheet.write(row, col, sc, head_format)
-                col += 1
             if head_schema.get(sheet_name):
                 for sc in head_schema[sheet_name]:
                     sheet.write(row, col, sc, head_format)
                     col += 1
 
-    def print_common(self, workbook, sheet, prj, row, col):
+    def print_ctg(self, workbook, sheet, prj, row, col):
 
         row_format = workbook.add_format({
             'font_size': 10,
@@ -86,6 +106,10 @@ class ReportContractingRevenueCashExcel(models.AbstractModel):
         row_format_number = workbook.add_format({
             'font_size': 10,
             'num_format': '#,##0',
+        })
+        row_format_date = workbook.add_format({
+            'font_size': 10,
+            'num_format': 'dd.mm.yyyy',
         })
 
         sheet.write(row, col, prj.project_id, row_format)
@@ -123,22 +147,6 @@ class ReportContractingRevenueCashExcel(models.AbstractModel):
         col += 1
         sheet.write(row, col, prj.profitability, row_format_number)
         col += 1
-        return col
-
-    def print_ctg(self, workbook, sheet, prj, row, col):
-
-        row_format = workbook.add_format({
-            'font_size': 10,
-        })
-        row_format_number = workbook.add_format({
-            'font_size': 10,
-            'num_format': '#,##0',
-        })
-        row_format_date = workbook.add_format({
-            'font_size': 10,
-            'num_format': 'dd.mm.yyyy',
-        })
-
         sheet.write(row, col, prj.end_presale_project_month, row_format_date)
         col += 1
         sheet.write(row, col, self.get_quater_from_month(prj.end_presale_project_month.month) + ' ' + str(prj.end_presale_project_month.year), row_format)
@@ -160,6 +168,46 @@ class ReportContractingRevenueCashExcel(models.AbstractModel):
             'num_format': 'dd.mm.yyyy',
         })
 
+        sheet.write(row, col, prj.project_id, row_format)
+        col += 1
+        if rvn_type == 'План':
+            sheet.write(row, col, str(rvn.id) + 'П', row_format)
+        else:
+            sheet.write(row, col, str(rvn.id) + 'Ф', row_format)
+        col += 1
+        sheet.write(row, col, prj.step_project_number or '', row_format)
+        col += 1
+        sheet.write(row, col, prj.responsibility_center_id.name, row_format)
+        col += 1
+
+        if prj.order_ids:
+            pn = ', '.join(cat.root_category_id.name if cat.root_category_id else cat.name for cat in prj.order_ids.line_ids.product_category_id)
+            sheet.write(row, col, pn, row_format)
+        else:
+            sheet.write(row, col, '', row_format)
+        col += 1
+
+        if prj.order_ids:
+            rpn = ', '.join(cat.root_category_id.head_id.name if cat.root_category_id else cat.head_id.name for cat in prj.order_ids.line_ids.product_category_id if (cat.root_category_id.head_id if cat.root_category_id else cat.head_id))
+            sheet.write(row, col, rpn, row_format)
+        else:
+            sheet.write(row, col, '', row_format)
+        col += 1
+
+        sheet.write(row, col, prj.key_account_manager_id.name or '', row_format)
+        col += 1
+        sheet.write(row, col, prj.project_manager_id.name or '', row_format)
+        col += 1
+        sheet.write(row, col, prj.partner_id.name or '', row_format)
+        col += 1
+        sheet.write(row, col, prj.company_partner_id.partner_id.name or '', row_format)
+        col += 1
+        sheet.write(row, col, prj.essence_project or '', row_format)
+        col += 1
+        sheet.write(row, col, prj.stage_id.name, row_format)
+        col += 1
+        sheet.write(row, col, prj.profitability, row_format_number)
+        col += 1
         sheet.write(row, col, rvn.date_cash, row_format_date)
         col += 1
         sheet.write(row, col, self.get_quater_from_month(rvn.date_cash.month) + ' ' + str(rvn.date_cash.year), row_format)
@@ -183,6 +231,46 @@ class ReportContractingRevenueCashExcel(models.AbstractModel):
             'num_format': 'dd.mm.yyyy',
         })
 
+        sheet.write(row, col, prj.project_id, row_format)
+        col += 1
+        if csh_type == 'План':
+            sheet.write(row, col, str(csh.id) + 'П', row_format)
+        else:
+            sheet.write(row, col, str(csh.id) + 'Ф', row_format)
+        col += 1
+        sheet.write(row, col, prj.step_project_number or '', row_format)
+        col += 1
+        sheet.write(row, col, prj.responsibility_center_id.name, row_format)
+        col += 1
+
+        if prj.order_ids:
+            pn = ', '.join(cat.root_category_id.name if cat.root_category_id else cat.name for cat in prj.order_ids.line_ids.product_category_id)
+            sheet.write(row, col, pn, row_format)
+        else:
+            sheet.write(row, col, '', row_format)
+        col += 1
+
+        if prj.order_ids:
+            rpn = ', '.join(cat.root_category_id.head_id.name if cat.root_category_id else cat.head_id.name for cat in prj.order_ids.line_ids.product_category_id if (cat.root_category_id.head_id if cat.root_category_id else cat.head_id))
+            sheet.write(row, col, rpn, row_format)
+        else:
+            sheet.write(row, col, '', row_format)
+        col += 1
+
+        sheet.write(row, col, prj.key_account_manager_id.name or '', row_format)
+        col += 1
+        sheet.write(row, col, prj.project_manager_id.name or '', row_format)
+        col += 1
+        sheet.write(row, col, prj.partner_id.name or '', row_format)
+        col += 1
+        sheet.write(row, col, prj.company_partner_id.partner_id.name or '', row_format)
+        col += 1
+        sheet.write(row, col, prj.essence_project or '', row_format)
+        col += 1
+        sheet.write(row, col, prj.stage_id.name, row_format)
+        col += 1
+        sheet.write(row, col, prj.profitability, row_format_number)
+        col += 1
         sheet.write(row, col, csh.date_cash, row_format_date)
         col += 1
         sheet.write(row, col, self.get_quater_from_month(csh.date_cash.month) + ' ' + str(csh.date_cash.year), row_format)
@@ -225,31 +313,26 @@ class ReportContractingRevenueCashExcel(models.AbstractModel):
         for prj in projects:
             if prj.end_presale_project_month.year == year:
                 row_ctg += 1
-                col = self.print_common(workbook, sheets['contracting'], prj, row_ctg, 0)
-                self.print_ctg(workbook, sheets['contracting'], prj, row_ctg, col)
+                self.print_ctg(workbook, sheets['contracting'], prj, row_ctg, 0)
 
             for rvn in prj.planned_acceptance_flow_ids:
                 if rvn.date_cash.year == year:
                     row_rvn += 1
-                    col = self.print_common(workbook, sheets['revenue'], prj, row_rvn, 0)
-                    self.print_rvn(workbook, sheets['revenue'], prj, rvn, row_rvn, col, 'План')
+                    self.print_rvn(workbook, sheets['revenue'], prj, rvn, row_rvn, 0, 'План')
             for rvn in prj.fact_acceptance_flow_ids:
                 if rvn.date_cash.year == year:
                     row_rvn += 1
-                    col = self.print_common(workbook, sheets['revenue'], prj, row_rvn, 0)
-                    self.print_rvn(workbook, sheets['revenue'], prj, rvn, row_rvn, col, 'Факт')
+                    self.print_rvn(workbook, sheets['revenue'], prj, rvn, row_rvn, 0, 'Факт')
 
 
             for csh in prj.planned_cash_flow_ids:
                 if csh.date_cash.year == year:
                     row_csh += 1
-                    col = self.print_common(workbook, sheets['cash'], prj, row_csh, 0)
-                    self.print_csh(workbook, sheets['cash'], prj, csh, row_csh, col, 'План')
+                    self.print_csh(workbook, sheets['cash'], prj, csh, row_csh, 0, 'План')
             for csh in prj.fact_cash_flow_ids:
                 if csh.date_cash.year == year:
                     row_csh += 1
-                    col = self.print_common(workbook, sheets['cash'], prj, row_csh, 0)
-                    self.print_csh(workbook, sheets['cash'], prj, csh, row_csh, col, 'Факт')
+                    self.print_csh(workbook, sheets['cash'], prj, csh, row_csh, 0, 'Факт')
 
 
     def generate_xlsx_report(self, workbook, data, budgets):
